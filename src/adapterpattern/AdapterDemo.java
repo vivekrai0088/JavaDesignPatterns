51e7c835927cfab113daea1a0a51a2754aa9dcd4
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-04-17 23:05:08 +0530

    adapter pattern

--- src/adapterpattern/AdapterDemo.java
+++ src/adapterpattern/AdapterDemo.java
@@ -1,5 +1,8 @@
 package adapterpattern;
 
+// The Adapter pattern is a structural design pattern that allows incompatible classes to work together by wrapping an
+// interface around an existing class. The adapter acts as a bridge between the client and the target class, translating
+// requests from the client into a format that the target class can understand.
 public class AdapterDemo {
 
   public static void main(String[] args) {
